5787ea5c87f42bc7844e55d25a54974d5800a3c2
nguyentuananh921 <nguyentuananh921@gmail.com>
2021-01-06 16:45:11 +0700

    Try to create partitial viewCarregistration

--- DataBase/SQL Command/CarPeople.sql
+++ DataBase/SQL Command/CarPeople.sql
@@ -4,7 +4,7 @@ from CarDrivers,Cars,Peoples
 
 where Cars.CarID=CarDrivers.CarID and CarDrivers.PeopleID=Peoples.PeopleID and CarPlate='30A-815.89'
 
-select * from Cars where CarPlate='29A-157.52' 
+select * from Cars where CarPlate='30E-580.66' 
 
 update Cars set PeopleID=317 where CarID=38
 update Cars set CarModelID=null where CarPlate='29A-157.52'
